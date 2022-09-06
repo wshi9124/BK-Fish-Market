a02801a8e933c2f50653c7218cf7ee76988cd6f1
Willie Shi <wshi9124@gmail.com>
2022-09-05 23:32:38 -0400

    margins

--- client/src/pages/AboutUs/ContactUs.tsx
+++ client/src/pages/AboutUs/ContactUs.tsx
@@ -2,9 +2,46 @@ import React from 'react';
 
 function ContactUs() {
   return (
-    <div className="w-1/3 mb-20">
-      <p className="text-5xl">Contact us / Store Hours</p>
+    <div className="w-2/5 mb-20">
       <div className="text-2xl">
+        <p className="mt-3 text-5xl">Store Hours</p>
+        <p className="mt-5">
+          <strong>Monday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          {' '}
+          <strong>Tuesday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          <strong>Wednesday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          <strong>Thursday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          <strong>Friday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          <strong>Saturday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="mt-3">
+          <strong>Sunday:</strong>
+          {' '}
+          9:00 AM - 9:30 PM
+        </p>
+        <p className="text-5xl mt-10">Contact us</p>
         <p className="mt-5">
           <strong>Phone Number:</strong>
           {' '}
@@ -15,14 +52,11 @@ function ContactUs() {
           {' '}
           bkfishminimarket@gmail.com
         </p>
-        <p className="mt-3 font-bold">Store Hours</p>
-        <p className="mt-3">Monday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3"> Tuesday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3">Wednesday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3">Thursday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3">Friday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3"> Saturday 9:00 AM - 9:30 PM</p>
-        <p className="mt-3">Sunday 9:00 AM - 9:30 PM</p>
+        <p className="mt-3">
+          <strong>Address:</strong>
+          {' '}
+          1161 Broadway Brooklyn NY 11221
+        </p>
       </div>
     </div>
   );
--- client/src/pages/AboutUs/GoogleMaps.tsx
+++ client/src/pages/AboutUs/GoogleMaps.tsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function GoogleMaps() {
   return (
-    <div className="w-2/3">
-      <p className="text-5xl">Location</p>
-      <p className="text-2xl mt-5">
-        <strong>Address:</strong>
-        {' '}
-        1161 Broadway Brooklyn NY 11221
-      </p>
+    <div className="w-3/5">
+      <p className="text-5xl mt-3">Location</p>
     </div>
   );
 }
