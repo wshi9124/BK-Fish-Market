513cc6b90479fd50997b27585ecc45e1da21fdbc
Willie Shi <wshi9124@gmail.com>
2022-09-07 00:43:52 -0400

    refactored code

---
+++ client/src/pages/Admin/AdminCategorySidebar.tsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { IProduct } from '../../types/IProducts';
+
+interface Props {
+  setProductData:React.Dispatch<React.SetStateAction<[IProduct]>>
+}
+
+function AdminCategorySidebar({ setProductData }:Props) {
+  const handleAllProducts = () => {
+    fetch('/products')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((data) => {
+              setProductData(data);
+            });
+        }
+      });
+  };
+
+  const handleSteakOrFillet = () => {
+    fetch('/products')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((data) => {
+              const allSteaks = data.filter((item:IProduct) => item.category === 'Steak or fillet');
+              setProductData(allSteaks);
+            });
+        }
+      });
+  };
+
+  const handleWholeFish = () => {
+    fetch('/products')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((data) => {
+              const allWholeFish = data.filter((item:IProduct) => item.category === 'Whole fish');
+              setProductData(allWholeFish);
+            });
+        }
+      });
+  };
+
+  const handleShellfish = () => {
+    fetch('/products')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((data) => {
+              const allShellfish = data.filter((item:IProduct) => item.category === 'Shellfish');
+              setProductData(allShellfish);
+            });
+        }
+      });
+  };
+
+  const handleOther = () => {
+    fetch('/products')
+      .then((res) => {
+        if (res.ok) {
+          res.json()
+            .then((data) => {
+              const allOther = data.filter((item:IProduct) => item.category === 'Other');
+              setProductData(allOther);
+            });
+        }
+      });
+  };
+  return (
+    <div className="flex flex-col w-1/5 text-center text-3xl text-black mx-5">
+      <p className="text-white bg-slate-900 rounded-t-lg py-4 font-bold">Categories</p>
+      <button
+        className="py-3 hover:text-red-400 border"
+        type="button"
+        onClick={handleAllProducts}
+      >
+        All Products
+
+      </button>
+      <button
+        className="py-5 hover:text-red-400 border"
+        type="button"
+        onClick={handleSteakOrFillet}
+      >
+        Steak or Fillet
+
+      </button>
+      <button
+        className="py-5 hover:text-red-400 border"
+        type="button"
+        onClick={handleWholeFish}
+      >
+        Whole Fish
+
+      </button>
+      <button
+        className="py-5  hover:text-red-400 border"
+        type="button"
+        onClick={handleShellfish}
+      >
+        Shellfish
+
+      </button>
+      <button
+        className="py-5 rounded-b-lg hover:text-red-400 border"
+        type="button"
+        onClick={handleOther}
+      >
+        Other
+
+      </button>
+    </div>
+
+  );
+}
+
+export default AdminCategorySidebar;
--- client/src/pages/ProductsPage/ProductPageFishCard.tsx
+++ client/src/pages/ProductsPage/ProductPageFishCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable  @typescript-eslint/no-non-null-assertion */
 import React from 'react';
 import { IProduct } from '../../types/IProducts';
+import ProductCategorySidebar from './ProductCategorySidebar';
 import currencyFormat from '../../libs/Util';
 
 interface Props {
@@ -33,116 +34,10 @@ function ProductPageFishCard({ productData, setProductData, search }: Props) {
     </div>
   ));
 
-  const handleAllProducts = () => {
-    fetch('/products')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              setProductData(data);
-            });
-        }
-      });
-  };
-
-  const handleSteakOrFillet = () => {
-    fetch('/products')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              const allSteaks = data.filter((item:IProduct) => item.category === 'Steak or fillet');
-              setProductData(allSteaks);
-            });
-        }
-      });
-  };
-
-  const handleWholeFish = () => {
-    fetch('/products')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              const allWholeFish = data.filter((item:IProduct) => item.category === 'Whole fish');
-              setProductData(allWholeFish);
-            });
-        }
-      });
-  };
-
-  const handleShellfish = () => {
-    fetch('/products')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              const allShellfish = data.filter((item:IProduct) => item.category === 'Shellfish');
-              setProductData(allShellfish);
-            });
-        }
-      });
-  };
-
-  const handleOther = () => {
-    fetch('/products')
-      .then((res) => {
-        if (res.ok) {
-          res.json()
-            .then((data) => {
-              const allOther = data.filter((item:IProduct) => item.category === 'Other');
-              setProductData(allOther);
-            });
-        }
-      });
-  };
-
   return (
     <div className="mt-10">
       <div className="flex">
-        <div className="flex flex-col w-1/5 text-center text-3xl text-black mx-5">
-          <p className="text-white bg-slate-900 rounded-t-lg py-4 font-bold">Categories</p>
-          <button
-            className="py-3 hover:text-red-400 border"
-            type="button"
-            onClick={handleAllProducts}
-          >
-            All Products
-
-          </button>
-          <button
-            className="py-5 hover:text-red-400 border"
-            type="button"
-            onClick={handleSteakOrFillet}
-          >
-            Steak or Fillet
-
-          </button>
-          <button
-            className="py-5 hover:text-red-400 border"
-            type="button"
-            onClick={handleWholeFish}
-          >
-            Whole Fish
-
-          </button>
-          <button
-            className="py-5  hover:text-red-400 border"
-            type="button"
-            onClick={handleShellfish}
-          >
-            Shellfish
-
-          </button>
-          <button
-            className="py-5 rounded-b-lg hover:text-red-400 border"
-            type="button"
-            onClick={handleOther}
-          >
-            Other
-
-          </button>
-        </div>
+        <ProductCategorySidebar setProductData={setProductData} />
         <div className="grid lg:grid-cols-4 sm:grid-cols-2 w-4/5 gap-5 mb-10 mr-10">
           {renderProducts}
         </div>
