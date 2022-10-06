deff187c4fb2356d939f64d11d39c4061912a601
Willie Shi <wshi9124@gmail.com>
2022-10-06 15:48:22 -0400

    logo

--- client/src/CommonComponents/Logo.tsx
+++ client/src/CommonComponents/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo() {
   return (
     <header className="relative w-1/3 content-center mx-auto">
-      <img src="/logo.jpeg" alt="logo" />
+      <img src="/logoHeader.jpeg" alt="logo" />
     </header>
   );
 }
